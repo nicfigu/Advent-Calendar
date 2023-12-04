a3ecd5475e25c7f9aee399a02e96c500a346adff
nicfigu <nicfigu@gmail.com>
2023-12-04 15:13:42 -0500

    print statement

--- Day4/solution.py
+++ Day4/solution.py
@@ -9,7 +9,7 @@ def get_card_points(line):
     win_set = set()
     for i in win_nums_arr:
         win_set.add(i)
-    my_nums = line[end_index+2, len(line)]
+    my_nums = line[end_index+2: len(line)]
     my_nums = my_nums.split()
     count = 0
     points = 0
@@ -20,16 +20,17 @@ def get_card_points(line):
                 points = 1
             else:
                 points *= 2
-    return points
+    return [points, count]
 def main():
     total_points = 0
-    file = open("input.txt", 'r')
+    file = open("Day4\input.txt", 'r')
     while True:
         content=file.readline()
         if not content:
             break
-        total_points += get_card_points(content)
-    print(total_points)
+        total_points += get_card_points(content)[0]
+    print("Total points (part 1): ", int(total_points))
+
 
 if __name__ == "__main__":
     main()
