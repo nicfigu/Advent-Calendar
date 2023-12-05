3cd2d906967ef47632c86a4e60c11cf1a7995404
nicfigu <nicfigu@gmail.com>
2023-12-04 21:56:01 -0500

    Part 2!

--- Day4/solution.py
+++ Day4/solution.py
@@ -21,15 +21,26 @@ def get_card_points(line):
             else:
                 points *= 2
     return [points, count]
+def num_of_scorecards(count, game_num, my_array):
+    for i in range(1, count+1):
+        if game_num+i in range(len(my_array)):
+            my_array[game_num+i] += my_array[game_num]
+
 def main():
     total_points = 0
     file = open("Day4\input.txt", 'r')
+    game_num = 0
+    arr_of_scorecard_count = [1] * 208
     while True:
         content=file.readline()
         if not content:
             break
-        total_points += get_card_points(content)[0]
+        ans = get_card_points(content)
+        total_points += ans[0]
+        num_of_scorecards(ans[1], game_num, arr_of_scorecard_count)
+        game_num +=1
     print("Total points (part 1): ", int(total_points))
+    print("Total number of scorecards: " + str(sum(arr_of_scorecard_count)))
 
 
 if __name__ == "__main__":
